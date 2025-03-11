e88b4615557eb321dc688576efbef540e5926fab
AL EMRAN <emrancu1@gmail.com>
2025-03-11 19:14:10 +0600

    update

--- src/Supports/DomainSupport.php
+++ src/Supports/DomainSupport.php
@@ -22,7 +22,51 @@ class DomainSupport
         $this->cacheData();
     }
 
-    private function arrayToString($array, $indent = 0) {
+    private function cacheData()
+    {
+        $content = $this->data();
+
+        $returnStatement = $this->arrayToString($content, 1);
+
+        $contentFinal = "<?php\n\n";
+        $contentFinal .= "return [\n" . $returnStatement . "];\n";
+
+        $this->seeders = $content['seeders'];
+        $this->factories = $content['factories'];
+        $this->domains = $content['domains'];
+    }
+
+    protected function data(): array
+    {
+        /** @var array{ factories: array, domains: array, seeders: array } $cachedData */
+
+        $composerData = json_decode(file_get_contents(base_path('composer.json')), true);
+        $autoload = $composerData['autoload']['psr-4'] ?? [];
+
+        ksort($autoload);
+        $seeders = [];
+        $domains = ["app" => ['title' => 'app', 'namespace' => 'App\\', 'real_path' => 'app']];
+        $factories = [];
+
+        $allDomainFiles = glob(base_path('domains') . '/*/index.php', GLOB_NOSORT);
+
+        foreach ($allDomainFiles as $file) {
+            $config = include $file;
+            $split = explode('/', $file);
+
+            $title = $split[count($split) - 2];
+
+            $domains[$title] = ['title' => $title, 'namespace' => array_keys($config['autoload'])[0], 'real_path' => 'domains' . DIRECTORY_SEPARATOR . $title . DIRECTORY_SEPARATOR . 'app',];
+
+//            dd($domains);
+        }
+
+
+        return ['factories' => $factories, 'domains' => $domains, 'seeders' => $seeders,];
+    }
+
+    private function arrayToString($array, $indent = 0)
+    {
         $output = '';
 
         foreach ($array as $key => $value) {
@@ -38,7 +82,6 @@ class DomainSupport
         return $output;
     }
 
-
     public static function clearData()
     {
         $storagePath = storage_path('app/domain-data.php');
@@ -46,81 +89,6 @@ class DomainSupport
         if (File::exists($storagePath)) {
             File::delete($storagePath);
         }
-    }
-
-
-    private function cacheData()
-    {
-        $storagePath = storage_path('app/domain-data.php');
-
-        if (!file_exists($storagePath)) {
-
-            File::ensureDirectoryExists(storage_path('app'));
-
-            $content = $this->data();
-
-            $returnStatement = $this->arrayToString($content, 1);
-
-            $contentFinal = "<?php\n\n";
-            $contentFinal .= "return [\n" . $returnStatement . "];\n";
-
-            $this->seeders = $content['seeders'];
-            $this->factories = $content['factories'];
-            $this->domains = $content['domains'];
-
-            if (file_put_contents($storagePath, $contentFinal) !== false) {
-            }
-
-            return;
-        }
-
-        $content =  include $storagePath;
-        $this->seeders = $content['seeders'];
-        $this->factories = $content['factories'];
-        $this->domains = $content['domains'];
-    }
-
-    protected function data(): array
-    {
-        /** @var array{ factories: array, domains: array, seeders: array } $cachedData */
-
-        $composerData = json_decode(file_get_contents(base_path('composer.json')), true);
-        $autoload = $composerData['autoload']['psr-4'] ?? [];
-
-        ksort($autoload);
-        $seeders = [];
-        $domains = [
-          "app" => [
-                'title' => 'app',
-                'namespace' => 'App\\',
-                'real_path' => 'app'
-          ]
-        ];
-        $factories = [];
-
-        $allDomainFiles = glob(base_path('domains').'/*/index.php', GLOB_NOSORT);
-
-        foreach ($allDomainFiles as $file) {
-            $config = include $file;
-            $split = explode('/', $file);
-
-            $title = $split[count($split)-2];
-            
-            $domains[$title] = [
-                'title' => $title,
-                'namespace' => array_keys($config['autoload'])[0],
-                'real_path' => 'domains'.DIRECTORY_SEPARATOR.$title.DIRECTORY_SEPARATOR.'app',
-            ];
-
-//            dd($domains);
-        }
-
-
-        return [
-            'factories' => $factories,
-            'domains' => $domains,
-            'seeders' => $seeders,
-        ];
     }
 
     public static function init($withoutCache = false): DomainSupport
